841c516520e743f2e2de3bb59f7cf9d14c993cfd
Sascha Diotte <diotte.sd@gmail.com>
2023-03-06 23:03:32 -0500

    Tkinter Menu Changes

--- ImportMenu.py
+++ ImportMenu.py
@@ -1,42 +1,90 @@
 import tkinter as tk
 from tkinter import *
-
-def UploadAction(event=None):
-    filename = filedialog.askopenfilename()
-    print('Selected:', filename)
+from tkinter import filedialog
+from tkinter import ttk
 
 window = tk.Tk()
-#root = Tk()
 
-#Menu Formatting
-from tkinter import ttk
-from tkinter import filedialog
+#Defining Functions
+def menu_bar(): 
+    pass
+
+def IMDSD_UploadAction(event=None):
+    for x in range(2):
+        filename = filedialog.askopenfilename(title='IMDSD Files')
+        print('Selected:', filename)
+    IMDSD_Reports = IMDSD()
+    Developer_File = Developer()
+    
+def CT2020_UploadAction(event=None):
+    for x in range(2):
+        filename = filedialog.askopenfilename(title='CT2020 Files')
+        print('Selected:', filename)
+        CT2020_Reports = CT2020()
+        Developer_File = Developer()
+
+def Systems_UploadAction(event=None):
+    for x in range(2):
+        filename = filedialog.askopenfilename(title='IMDSD Files')
+        print('Selected:', filename)
+    for x in range(2):
+        filename = filedialog.askopenfilename(title='CT2020 Files')
+        print('Selected:', filename)
+        Both_Reports = IMDSD()
+        Developer_File = Developer()
+
+def IMDSD(event=None):
+    pass
+#car = {
+  #"brand": "Ford",
+ # "model": "Mustang",
+ # "year": 1964
+#}
+
+#x = car.get("model")
+
+#print(x)
+
+def CT2020(event=None):
+    pass
+
+def Both(event=None):
+    pass
+
+def Developer(event=None):
+    top = Toplevel(window)
+    top.title('Developer File')
+    label.Label(top, text="Do you need a Developer File with this as well?", font=("Courier 22 bold"))
+    label.pack()
+#Create an Entry widget to accept User Input
+    global entry
+    entry= Entry(top, width= 40)
+    entry.focus_set()
+    entry.pack()
+    string= entry.get()
+    label.configure(text=string)
+#Create a Button to validate Entry Widget
+    ttk.Button(top, text= "Done",width= 20, command=Developer).pack(pady=20)
+
+#Menu Window Formatting
 window.title("Chaintrack Replacement")
-for x in range(8):
-   for y in range(4):
-       frame = tk.Frame(
-           master=window,
-           relief=tk.RAISED
-       )
 
-#Menu Buttons
-frame.grid(row=x, column=y)
-tk.Label(frame, text =f'\t\t\t\tChaintrack Replacement', font=('TkDefaultFont', 20)).grid (column=1, row=0) 
-tk.Label(frame, text=f"\n\nUpload IMDSD FMT File\n\n", font= "1").grid(column=1, row=1)
-tk.Button(frame, text="FMT", command=UploadAction, font= "1").grid(column=2, row=1)
-tk.Label(frame, text=f"\n\nUpload IMDSD DAT File\n\n", font= "1").grid(column=1, row=2)
-tk.Button(frame, text="DAT", command=UploadAction, font= "1").grid(column=2, row=2)
-tk.Label(frame, text=f"\n\nUpload CT2020 FMT File\n\n", font= "1").grid(column=1, row=3)
-tk.Button(frame, text="FMT", command=UploadAction, font= "1").grid(column=2, row=3)
-tk.Label(frame, text=f"\n\nUpload CT2020 DAT File\n\n", font= "1").grid(column=1, row=4)
-tk.Button(frame, text="DAT", command=UploadAction, font= "1").grid(column=2, row=4)
-tk.Label(frame, text=f"\n\nDone Uploading\n\n", font= "1").grid(column=1, row=5)
-tk.Button(frame, text="Quit", command=window.destroy, font= "1").grid(column=2, row=5)
+#Menubar Formmatting
+menubar=tk.Menu(window)
+menu_file=tk.Menu(menubar, tearoff=0)
 
-#Menu Checkbuttons
-tk.Checkbutton(frame, text = 'Parse IMDSD', onvalue = 1, offvalue = 0).grid(column=0, row=7)
-tk.Checkbutton(frame, text = f'Parse CT2020', onvalue = 1, offvalue = 0).grid(column=1, row=7)
-tk.Checkbutton(frame, text = 'Parse and Compare Both', onvalue = 1, offvalue = 0).grid(column=2, row=7)
-tk.Checkbutton(frame, text = 'Developer File', onvalue = 1, offvalue = 0).grid(column=3, row=7)
+menubar.add_cascade(label='File',menu=menu_file)
+
+#Submenu Formatting
+submenu_upload=tk.Menu(menu_file, tearoff=0)
+submenu_upload= Menu(menu_file, tearoff=0)
+submenu_upload.add_command(label="IMDSD DAT & FMT", command=IMDSD_UploadAction)
+submenu_upload.add_command(label="CT2020 DAT & FMT", command=CT2020_UploadAction)
+submenu_upload.add_command(label="Both", command=Systems_UploadAction)
+menu_file.add_cascade(label='Upload', menu=submenu_upload)
+
+menu_file.add_command(label='Exit', command=window.destroy)
+
+window.config(menu=menubar)
 
 window.mainloop()
