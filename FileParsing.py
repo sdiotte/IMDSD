2a761c2e574411c9dd54ae19ffd80c3cef6a10a0
Sascha Diotte <diotte.sd@gmail.com>
2023-03-12 11:00:00 -0400

    Save DAT and FMT Files to CWD

--- FileParsing.py
+++ FileParsing.py
@@ -8,5 +8,14 @@ with open('BATERR.FMT') as fmt:
     contents = fmt.read()
     print(contents)
 
+#Import text files
+def source():
+    
+with open (source) as source:
+    contents = source.read()
+    print (contents)
+#add code: if file is fmt, remove #'s and anything after...perhaps have it import the old fields and place under new fields on header. Maybe with acronym for system in the front with dot or dash
+
+    
 #DAT to FMT Array
-dat_to_fmt = []
+#dat_to_fmt = []
--- ImportMenu.py
+++ ImportMenu.py
@@ -1,29 +1,28 @@
+#Import Modules
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
+import os.path
+import shutil
 
 window = tk.Tk()
 
 #Defining Functions
-def IMDSD_UploadAction(event=None):
-    for x in range(2):
-        filename = filedialog.askopenfilename(title='IMDSD Files')
-        print('Selected:', filename)
-    IMDSD_Reports = IMDSD()
-    Developer_File = Developer_PopUp()
-    
-def CT2020_UploadAction(event=None):
-    for x in range(2):
-        filename = filedialog.askopenfilename(title='CT2020 Files')
-        print('Selected:', filename)
-        CT2020_Reports = CT2020()
-        Developer_File = Developer_PopUp()
 
-def Dev_File_UploadAction(event=None):
+def UploadAction(event=None):
     for x in range(2):
-        filename = filedialog.askopenfilename(title='Developer File')
-        print('Selected:', filename)
+        file = filedialog.askopenfilename(title=type)
+        filename = os.path.basename(file)
+        currentdirc = os.getcwd()
+        filepath = os.path.join(currentdirc, filename)
+        shutil.copy(file, filepath)
+
+    if type == 'IMDSD':
+        reports = IMDSD()
+    elif type == 'CT2020':
+        reports = CT2020()
+    print(type)
 
 def Systems_UploadAction(event=None):
     for x in range(2):
@@ -32,38 +31,25 @@ def Systems_UploadAction(event=None):
     for x in range(2):
         filename = filedialog.askopenfilename(title='CT2020 Files')
         print('Selected:', filename)
-        Both_Reports = Both()
-        Developer_File = Developer_PopUp()
+        Both_Reports = IMDSD()
 
 def IMDSD(event=None):
     pass
+#car = {
+  #"brand": "Ford",
+ # "model": "Mustang",
+ # "year": 1964
+#}
+
+#x = car.get("model")
+
+#print(x)
 
 def CT2020(event=None):
     pass
 
-def Dev(event=None):
-    pass
-
 def Both(event=None):
     pass
-
-def Developer_PopUp(event=None):
-    top = Toplevel(window)
-    top.title('Developer File')
-#Create label for User Input
-    var = StringVar()
-    label = Label(top, textvariable=var, font=("Times 12 bold"))
-    
-    var.set(f"Do you need a Developer File with this?")
-    label.pack(pady=15)
-#Create an Entry widget to accept User Input
-    global entry
-    entry= Entry(top, width= 40)
-    entry.focus_set()
-    entry.pack()
-    string= entry.get()
-#Create a Button to validate Entry Widget
-    ttk.Button(top, text= "Done",width= 20, command=Developer_PopUp).pack(pady=20)
 
 #Menu Window Formatting
 window.title("Chaintrack Replacement")
@@ -78,10 +64,10 @@ menubar.add_cascade(label='File', menu=menu_file)
 #Submenu Formatting
 submenu_upload=tk.Menu(menu_file, tearoff=0)
 submenu_upload= Menu(menu_file, tearoff=0)
-submenu_upload.add_command(label="IMDSD DAT & FMT", command=IMDSD_UploadAction)
-submenu_upload.add_command(label="CT2020 DAT & FMT", command=CT2020_UploadAction)
+#upload()
+submenu_upload.add_command(label="IMDSD DAT & FMT", command=UploadAction)
+submenu_upload.add_command(label="CT2020 DAT & FMT", command=UploadAction)
 submenu_upload.add_command(label="Both", command=Systems_UploadAction)
-submenu_upload.add_command(label="Developer File", command=Dev_File_UploadAction)
 menu_file.add_cascade(label='Upload', menu=submenu_upload)
 
 menu_file.add_command(label='Exit', command=window.destroy)
