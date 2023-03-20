4a7efb1bb14df46478e3efd365b715fe8e2f706f
Sascha Diotte <diotte.sd@gmail.com>
2023-03-19 20:48:47 -0400

    File Clean Up

--- FileParsing.py
+++ FileParsing.py
@@ -1,42 +1,41 @@
-#Import Modules
-import os,glob
+# Import Modules
+import os
 
-#List of files in current directory
+# List of files in current directory
 currentdirc = os.getcwd()
 
 Extensions = ['.dat', '.DAT', '.fmt', '.FMT']
 
+
 def get_file_list(root_dir, F):
     file_list = []
     counter = 1
-    
+
     for root, dirs, files in os.walk(root_dir):
         for files in files:
-            
+
             if any(ext in files for ext in F):
                 file_list.append(files)
                 counter += 1
     return file_list
 
+
 files = get_file_list(currentdirc, Extensions)
 print(files)
 
-#Read/Write Files
+# Read/Clean Files
+
+
 def readfiles(event=None):
-    
-    for filename in files:
-        with open(filename, 'r') as infile:
-            file = infile.read()
-            file = file.split('#', 1)[0] #START here to remove everything after '#' line by line
-        with open(filename, 'w') as outfile:
-            outfile.write(file)
-            print(file)
+    count = 0
+    for file in files:
+        with open(file, 'r+') as infile:
+            Lines = infile.read().splitlines()
+            for line in Lines:
+                 if line.startswith("#"):
+                      continue
+                 else:
+                      count += 1
+                      print(line)
 
 readfiles()
-
-#Clean Files
-def cleanfiles(event=None):
-    #for filename in files:
-        #filename.replace('#','')
-        #print(filename)
-        pass
